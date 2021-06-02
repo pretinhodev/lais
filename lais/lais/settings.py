2afa8563b3fee9ad5ee436703ba024c8595e9eaa
pretinhodev <38338362+pretinhodev@users.noreply.github.com>
2021-06-02 12:15:05 -0300

    configure core

--- lais/lais/settings.py
+++ lais/lais/settings.py
@@ -37,12 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'agendamento',
-    'cidadao',
-    'comprovante',
     'core',
-    'grupo',
-    'local',
 ]
 
 MIDDLEWARE = [
