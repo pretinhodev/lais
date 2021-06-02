dd72ca6cf5157a7e3de76da06efbcc249c73b7ef
pretinhodev <38338362+pretinhodev@users.noreply.github.com>
2021-06-02 11:26:24 -0300

    configure apps

--- lais/lais/settings.py
+++ lais/lais/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'agendamento',
+    'cidadao',
+    'comprovante',
     'core',
+    'grupo',
+    'local',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
