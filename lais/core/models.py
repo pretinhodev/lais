df4c15eb9159b3bec63108f91f01ec33336c26e5
pretinhodev <38338362+pretinhodev@users.noreply.github.com>
2021-06-02 15:35:36 -0300

    set and alter models

--- lais/core/models.py
+++ lais/core/models.py
@@ -12,3 +12,13 @@ class Cidadao(models.Model):
     data = models.DateField('Data de nascimento')
     email = models.EmailField('Email', max_length=254)
     senha = models.CharField('Senha', max_length=254)
+
+class Grupo(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    idade = models.PositiveSmallIntegerField('Idade')
+
+class Local(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    logradouro = models.CharField('Logradouro', max_length=100)
+    bairro = models.CharField('Bairro', max_length=25)
+    cidade = models.CharField('Cidade', max_length=25)
