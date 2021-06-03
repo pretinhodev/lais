93a6c4d67eaea40601fc913d7d764147b481fd46
pretinhodev <38338362+pretinhodev@users.noreply.github.com>
2021-06-02 22:20:17 -0300

    configure forms

--- lais/core/forms.py
+++ lais/core/forms.py
@@ -1,10 +1,8 @@
-from typing_extensions import Required
 from django import forms
-from django.forms import widgets
 
 class CidadaoForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100, required=True)
     data = forms.DateField(label='Data de nascimento', required=True)
     email = forms.EmailField(label='Email', required=True)
-    senha = forms.CharField(label='Senha', required=True, widgets=forms.PasswordInput())
-    confirmarsenha = forms.CharField(label='Confirmar senha', required=True, widgets=forms.PasswordInput())
+    senha = forms.CharField(label='Senha', required=True)
+    confirmarsenha = forms.CharField(label='Confirmar senha', required=True)
