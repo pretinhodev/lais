863235ed3617f75b03e248446c8d4a5fe3acb2b8
pretinhodev <38338362+pretinhodev@users.noreply.github.com>
2021-06-02 22:21:40 -0300

    cidadao form

--- lais/core/forms.py
+++ lais/core/forms.py
@@ -3,6 +3,6 @@ from django import forms
 class CidadaoForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100, required=True)
     data = forms.DateField(label='Data de nascimento', required=True)
-    email = forms.EmailField(label='Email', required=True)
-    senha = forms.CharField(label='Senha', required=True)
-    confirmarsenha = forms.CharField(label='Confirmar senha', required=True)
+    email = forms.EmailField(label='Email', max_length=254, required=True)
+    senha = forms.CharField(label='Senha', max_length=254, required=True)
+    confirmarsenha = forms.CharField(label='Confirmar senha', max_length=254, required=True)
